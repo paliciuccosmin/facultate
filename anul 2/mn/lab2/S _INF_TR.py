654d06f52bc10836bd408b50589c0fb8eb61a6bb
Cosmin <cosmincostantinp@gmail.com>
2023-10-26 01:07:28 +0300

    sa moara copiii mei ca nu mai pot

--- anul 2/mn/lab2/S _INF_TR.py
+++ anul 2/mn/lab2/S _INF_TR.py
@@ -1,6 +1,13 @@
 import numpy as np
+n=7
+rng=np.random.default_rng()
+A=np.array(rng.random(size=(n,n)))
+u=np.zeros((n,n))
+for k in range(n):
+    for i in range(k + 1, n):
+        u[i, k] = A[i, k] / A[k, k]
+        A[i, k] = u[i, k]
+    for j in range(k + 1, n):
+            A[i, j] -= u[i, k] * A[k, j]
 
-def s_inf_tr():
-    for i in range():
-        for j in range():
-            i=i+1
+print(A)
---
+++ anul 2/poo/lab3/lab3_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+class Money {
+
+private:
+	int amount;
+	static int count;
+	
+};
+
+int main()
+{
+	cout << "Hello world";
+}
